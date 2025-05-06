5d6080526db5957156d3f0cfcd4af3bda0c2d606
alejandro <alejandrovillamilcarmona@gmail.com>
2025-05-05 21:45:13 -0500

    Integración con API y vista del modal

--- src/modules/users/pages/UsersPage.tsx
+++ src/modules/users/pages/UsersPage.tsx
@@ -3,6 +3,7 @@ import { Table, Space, Button, Dropdown, Tag } from 'antd'
 import type { ColumnsType } from 'antd/es/table'
 import type { MenuProps } from 'antd'
 import { EditOutlined, DeleteOutlined, SyncOutlined, PlusOutlined } from '@ant-design/icons'
+import { UserFormModal } from '../components/userFormModal/UserFormModal'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:9009';
 
@@ -53,6 +54,8 @@ const fetchUsers = async (page: number, size: number, sortField: string, sortOrd
 const UsersPage = () => {
   const [data, setData] = useState<UserType[]>([])
   const [loading, setLoading] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editingUser, setEditingUser] = useState<UserType | null>(null);
   const [tableParams, setTableParams] = useState({
     pagination: {
       current: 1,
@@ -114,10 +117,7 @@ const UsersPage = () => {
       sortOrder: sorter.order ? sorter.order.replace('end', '') : 'asc'
     })
   }
-
-  const handleEdit = (record: UserType) => {
-    console.log('Editar:', record)
-  }
+ 
 
   const handleDelete = (record: UserType) => {
     console.log('Eliminar:', record)
@@ -206,228 +206,23 @@ const UsersPage = () => {
     },
   ]
 
-  const dataEx: UserType[] = [
-    {
-      key: '1',
-      id: 1,
-      username: 'JohnDuran',
-      nombre: 'John',
-      apellido: 'Duran',
-      email: 'John.Duran@gmail.com',
-      telefono: '3124567890',
-      rol: 'Administrador',
-      estado: 'Activo'
-    },
-    {
-      key: '2',
-      id: 2,
-      username: 'Alex89',
-      nombre: 'Alejandro',
-      apellido: 'Garcia',
-      email: 'alex.garcia@gmail.com',
-      telefono: '3104567890',
-      rol: 'Administrador',
-      estado: 'Activo'
-    },
-    {
-      key: '3',
-      id: 3,
-      username: 'LMarquez',
-      nombre: 'Laura',
-      apellido: 'Márquez',
-      email: 'laura.mqz@yahoo.com',
-      telefono: '3001234567',
-      rol: 'Profesor',
-      estado: 'Inactivo'
-    },
-    {
-      key: '4',
-      id: 4,
-      username: 'Vale_22',
-      nombre: 'Valentina',
-      apellido: 'Ríos',
-      email: 'valen.rios@gmail.com',
-      telefono: '3123456789',
-      rol: 'Profesor',
-      estado: 'Activo'
-    },
-    {
-      key: '5',
-      id: 5,
-      username: 'CAndres',
-      nombre: 'Carlos',
-      apellido: 'Andrade',
-      email: 'carlos.a@gmail.com',
-      telefono: '3208765432',
-      rol: 'Estudiante',
-      estado: 'Activo'
-    },
-    {
-      key: '6',
-      id: 6,
-      username: 'JLopez92',
-      nombre: 'Juliana',
-      apellido: 'López',
-      email: 'juli.lopez@hotmail.com',
-      telefono: '3197654321',
-      rol: 'Administrador de herramientas',
-      estado: 'Activo'
-    },
-    {
-      key: '7',
-      id: 7,
-      username: 'MAcevedo',
-      nombre: 'Manuel',
-      apellido: 'Acevedo',
-      email: 'm.acevedo@outlook.com',
-      telefono: '3182345678',
-      rol: 'Estudiante',
-      estado: 'Inactivo'
-    },
-    {
-      key: '8',
-      id: 8,
-      username: 'SofiaH',
-      nombre: 'Sofia',
-      apellido: 'Herrera',
-      email: 'sofiah@gmail.com',
-      telefono: '3134567890',
-      rol: 'Estudiante',
-      estado: 'Activo'
-    },
-    {
-      key: '9',
-      id: 9,
-      username: 'DaniGomez',
-      nombre: 'Daniel',
-      apellido: 'Gómez',
-      email: 'daniel.gomez@yahoo.com',
-      telefono: '3007894561',
-      rol: 'Estudiante',
-      estado: 'Activo'
-    },
-    {
-      key: '10',
-      id: 10,
-      username: 'NCastillo',
-      nombre: 'Natalia',
-      apellido: 'Castillo',
-      email: 'nat.castillo@live.com',
-      telefono: '3151239876',
-      rol: 'Estudiante',
-      estado: 'Activo'
-    },
-    {
-      key: '11',
-      id: 11,
-      username: 'JohnDuran',
-      nombre: 'John',
-      apellido: 'Duran',
-      email: 'John.Duran@gmail.com',
-      telefono: '3124567890',
-      rol: 'Administrador',
-      estado: 'Activo'
-    },
-    {
-      key: '12',
-      id: 12,
-      username: 'Alex89',
-      nombre: 'Alejandro',
-      apellido: 'Garcia',
-      email: 'alex.garcia@gmail.com',
-      telefono: '3104567890',
-      rol: 'Administrador',
-      estado: 'Activo'
-    },
-    {
-      key: '13',
-      id: 13,
-      username: 'LMarquez',
-      nombre: 'Laura',
-      apellido: 'Márquez',
-      email: 'laura.mqz@yahoo.com',
-      telefono: '3001234567',
-      rol: 'Profesor',
-      estado: 'Inactivo'
-    },
-    {
-      key: '14',
-      id: 14,
-      username: 'Vale_22',
-      nombre: 'Valentina',
-      apellido: 'Ríos',
-      email: 'valen.rios@gmail.com',
-      telefono: '3123456789',
-      rol: 'Profesor',
-      estado: 'Activo'
-    },
-    {
-      key: '15',
-      id: 15,
-      username: 'CAndres',
-      nombre: 'Carlos',
-      apellido: 'Andrade',
-      email: 'carlos.a@gmail.com',
-      telefono: '3208765432',
-      rol: 'Estudiante',
-      estado: 'Activo'
-    },
-    {
-      key: '16',
-      id: 16,
-      username: 'JLopez92',
-      nombre: 'Juliana',
-      apellido: 'López',
-      email: 'juli.lopez@hotmail.com',
-      telefono: '3197654321',
-      rol: 'Administrador de herramientas',
-      estado: 'Activo'
-    },
-    {
-      key: '17',
-      id: 17,
-      username: 'MAcevedo',
-      nombre: 'Manuel',
-      apellido: 'Acevedo',
-      email: 'm.acevedo@outlook.com',
-      telefono: '3182345678',
-      rol: 'Estudiante',
-      estado: 'Inactivo'
-    },
-    {
-      key: '18',
-      id: 18,
-      username: 'SofiaH',
-      nombre: 'Sofia',
-      apellido: 'Herrera',
-      email: 'sofiah@gmail.com',
-      telefono: '3134567890',
-      rol: 'Estudiante',
-      estado: 'Activo'
-    },
-    {
-      key: '19',
-      id: 19,
-      username: 'DaniGomez',
-      nombre: 'Daniel',
-      apellido: 'Gómez',
-      email: 'daniel.gomez@yahoo.com',
-      telefono: '3007894561',
-      rol: 'Estudiante',
-      estado: 'Activo'
-    },
-    {
-      key: '20',
-      id: 20,
-      username: 'NCastillo',
-      nombre: 'Natalia',
-      apellido: 'Castillo',
-      email: 'nat.castillo@live.com',
-      telefono: '3151239876',
-      rol: 'Estudiante',
-      estado: 'Activo'
-    },
-  ]
+  const handleCreateUser = (values: any) => {
+    // Implementar lógica para crear usuario
+    console.log('Crear usuario:', values);
+    setModalOpen(false);
+  };
+
+  const handleEditUser = (values: any) => {
+    // Implementar lógica para editar usuario
+    console.log('Editar usuario:', values, editingUser?.id);
+    setModalOpen(false);
+    setEditingUser(null);
+  };
+
+  const handleEdit = (record: UserType) => {
+    setEditingUser(record);
+    setModalOpen(true);
+  };
 
   return (
     <div style={{ padding: '24px' }}>
@@ -435,6 +230,7 @@ const UsersPage = () => {
       <div className='row mb-3'>
         <div className='col-12 d-flex justify-content-end'>
           <Button
+            onClick={() => setModalOpen(true)}
             style={{ backgroundColor: '#26B857', borderColor: '#26B857' }}
             type="primary"
             icon={<PlusOutlined />}
@@ -450,7 +246,17 @@ const UsersPage = () => {
         loading={loading}
         onChange={handleTableChange}
       />
-    </div>
+      <UserFormModal
+        open={modalOpen}
+        onClose={() => {
+          setModalOpen(false);
+          setEditingUser(null);
+        }}
+        onSubmit={editingUser ? handleEditUser : handleCreateUser}
+        initialValues={editingUser}
+        title={editingUser ? 'Editar Usuario' : 'Crear Usuario'}
+      />      
+    </div >
   )
 }
 
